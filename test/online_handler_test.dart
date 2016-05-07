3f3b3d0847c54d9a093b2591cc06427c766e092f
P.Y. Laligand <py.laligand@gmail.com>
2016-05-06 21:48:01 -0700

    Test for XurHandler
    Also grouped some standard testing tools into a single class.
    
    Fixes #63

--- test/online_handler_test.dart
+++ test/online_handler_test.dart
@@ -1,14 +1,13 @@
 // Copyright (c) 2016 P.Y. Laligand
 
-import 'dart:convert';
-
 import 'package:mockito/mockito.dart';
-import 'package:shelf/shelf.dart' as shelf;
 import 'package:test/test.dart';
 
 import '../lib/bungie_client.dart';
 import '../lib/context_params.dart' as param;
 import '../lib/online_handler.dart';
+
+import 'utils.dart';
 
 const _CLAN_ID = '123456789';
 const _ON_XBOX = true;
@@ -25,12 +24,12 @@ final _CHARACTER_TWO_OLD = new Character(
     'character_two', 'Warlock', _NOW.subtract(const Duration(days: 5)));
 
 void main() {
-  _MockBungieClient client;
+  MockBungieClient client;
   Map<String, dynamic> context;
   OnlineHandler handler;
 
   setUp(() {
-    client = new _MockBungieClient();
+    client = new MockBungieClient();
     context = {param.BUNGIE_CLIENT: client, param.SLACK_TEXT: 'xbl'};
     handler = new OnlineHandler(_CLAN_ID);
   });
@@ -43,14 +42,14 @@ void main() {
 
   test('no member in clan', () async {
     when(client.getClanRoster(_CLAN_ID, _ON_XBOX)).thenReturn(<ClanMember>[]);
-    final json = await _getResponse(handler, context);
+    final json = await getResponse(handler, context);
     expect(json['response_type'], equals('in_channel'));
     expect(json['attachments'], isNotNull);
   });
 
   test('invalid platform', () async {
     context[param.SLACK_TEXT] = 'bogus!';
-    final json = await _getResponse(handler, context);
+    final json = await getResponse(handler, context);
     expect(json['response_type'], isNot(equals('in_channel')));
     expect(json['text'], isNotNull);
     verifyZeroInteractions(client);
@@ -63,7 +62,7 @@ void main() {
         .thenReturn(_CHARACTER_ONE);
     when(client.getLastPlayedCharacter(_MEMBER_TWO.id))
         .thenReturn(_CHARACTER_TWO_OLD);
-    final json = await _getResponse(handler, context);
+    final json = await getResponse(handler, context);
     expect(json['response_type'], equals('in_channel'));
     expect(json['text'], isNotNull);
     expect(json['text'], contains(_MEMBER_ONE.gamertag));
@@ -77,7 +76,7 @@ void main() {
         .thenReturn(_CHARACTER_ONE_OLD);
     when(client.getLastPlayedCharacter(_MEMBER_TWO.id))
         .thenReturn(_CHARACTER_TWO_OLD);
-    final json = await _getResponse(handler, context);
+    final json = await getResponse(handler, context);
     expect(json['response_type'], equals('in_channel'));
     expect(json['attachments'], isNotNull);
   });
@@ -85,20 +84,8 @@ void main() {
   test('no character data', () async {
     when(client.getClanRoster(_CLAN_ID, _ON_XBOX))
         .thenReturn(<ClanMember>[_MEMBER_ONE, _MEMBER_TWO]);
-    final json = await _getResponse(handler, context);
+    final json = await getResponse(handler, context);
     expect(json['response_type'], equals('in_channel'));
     expect(json['attachments'], isNotNull);
   });
 }
-
-dynamic _getResponse(
-    OnlineHandler handler, Map<String, dynamic> context) async {
-  final request = new shelf.Request(
-      'POST', Uri.parse('http://something.com/path'),
-      context: context);
-  final response = await handler.handle(request);
-  expect(response.statusCode, equals(200));
-  return JSON.decode(await response.readAsString());
-}
-
-class _MockBungieClient extends Mock implements BungieClient {}
---
+++ test/utils.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2016 P.Y. Laligand
+
+import 'dart:convert';
+
+import 'package:mockito/mockito.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:test/test.dart';
+
+import '../lib/bungie_client.dart';
+import '../lib/bungie_database.dart';
+import '../lib/slack_command_handler.dart';
+
+dynamic getResponse(
+    SlackCommandHandler handler, Map<String, dynamic> context) async {
+  final request = new shelf.Request(
+      'POST', Uri.parse('http://something.com/path'),
+      context: context);
+  final response = await handler.handle(request);
+  expect(response.statusCode, equals(200));
+  return JSON.decode(await response.readAsString());
+}
+
+class MockBungieClient extends Mock implements BungieClient {}
+
+class MockBungieDatabase extends Mock implements BungieDatabase {}
