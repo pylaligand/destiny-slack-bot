b86d2e179ceb01e955b3d8fd2af35500d1dfb153
P.Y. Laligand <py.laligand@gmail.com>
2016-05-07 14:26:44 -0700

    Improved the Travis routine (#74)

---
+++ travis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) 2016 P.Y. Laligand
+
+set -e
+
+dart_files="bin/*.dart lib/*.dart test/*.dart tool/*.dart"
+
+pub run sqlite:install --package-root .
+dartanalyzer --fatal-warnings --fatal-hints --fatal-lints $dart_files
+pub run test
+
+unformatted_files=`dartfmt -n $dart_files`
+if [[ -n "$unformatted_files" ]]; then
+  echo "Some files need formatting:"
+  echo $unformatted_files
+  exit 314
+fi
