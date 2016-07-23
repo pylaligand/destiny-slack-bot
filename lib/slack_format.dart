731897f1d91518b497eba0173ebcbe034f6cb5f2
P.Y. Laligand <py.laligand@gmail.com>
2016-07-22 18:55:08 -0700

    Add link to MoT page on bungie.net
    Fixes #108

--- lib/slack_format.dart
+++ lib/slack_format.dart
@@ -5,11 +5,14 @@ import 'dart:convert';
 import 'package:shelf/shelf.dart' as shelf;
 
 /// Creates a response object with simple text content.
-shelf.Response createTextResponse(String content, {bool private: false}) {
+shelf.Response createTextResponse(String content,
+    {bool private: false, bool expandLinks: true}) {
   final json = new Map();
   if (!private) {
     json['response_type'] = 'in_channel';
   }
+  json['unfurl_media'] = expandLinks.toString();
+  json['unfurl_links'] = expandLinks.toString();
   json['text'] = content;
   final body = JSON.encode(json);
   final headers = {'content-type': 'application/json'};
